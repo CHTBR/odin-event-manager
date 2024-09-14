69380e5b45c5131f3e6cb781bac6d6fe3c6db0c7
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-14 18:55:14 +0200

    Add script to fit zipcodes into standard form

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,8 +8,13 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0, 5]
+end
+
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+  zipcode = clean_zipcode(zipcode)
   puts "#{name} - #{zipcode}"
 end
